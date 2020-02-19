de579bc8cbf87cf898b70c5a74c5b8616c824a33
kobako <zhuobinggang@gmail.com>
2020-02-19 15:14:32 +0900

    Minor fix

--- spiders/merica_item_spider.js
+++ spiders/merica_item_spider.js
@@ -127,12 +127,12 @@ function getSearchResult(keyword='', page=1){
   })
 }
 
-function isValidMid(mid){
+function isValidId(mid){
   return /(m[0-9]{8,})/.test(mid)
 }
 
 module.exports = {
   getItemInfo,
   getSearchResult,
-  isValidMid,
+  isValidId,
 }
